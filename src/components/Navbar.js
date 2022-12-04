b1ce070d523b01bb2203d025d4e76908d4bd99c0
joseph <josephogba247@gmail.com>
2022-12-04 21:32:20 +0530

    added an icon and image to the home screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {FaBars} from 'react-icons/fa'
 import office_building from './Images/office_building.jpg'
 
 
@@ -10,7 +11,7 @@ function Navbar() {
         <img src={office_building} alt ='office_building'/>
          </a>
        <div className= 'hamburger'>
-        
+         <FaBars />
        </div>
        </nav>
     </div>
